633f362a94bb1b1b60fafb7adacca536cb0a9c70
Pankaj Dhundhara <pankajdhundhara@192.168.1.4>
2022-05-07 03:28:26 +0530

    temp3

--- main.go
+++ main.go
@@ -1,18 +1,93 @@
 package main
 
+//package main => this is the part of the program we want to run when we start executeable
+//or it has the main function ,and the main function is the first thing that runs
+//init functions for packages will run first
+
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/PankajKumar9/lenslocked.com/controllers"
+	"github.com/PankajKumar9/lenslocked.com/models"
+
+	"github.com/PankajKumar9/lenslocked.com/views"
+	"github.com/gorilla/mux"
 )
 
-/* this is the function called anytime
-somebody comes to our webserver */
-func handlerFunc(w http.ResponseWriter, r *http.Request) { /* w http.ResponseWriter, this is where we write our response */
-	/*fprint me first argument is ,print kaha krna  h
-	  jaise yaha w pe krna h */
-	fmt.Fprint(w, "<h1> Welcome to my awesome site </h1>")
+var (
+	homeView    *views.View
+	contactView *views.View
+)
 
-	/*r *http.Request : has the information sort of about the user is making
-	  we can use that for sort of checking the user's ip address and stuff like that */
+// func home(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Contact-Type", "text/html")
+
+// 	must(homeView.Render(w, nil))
+// }
+// func contact(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Contact-Type", "text/html")
+// 	must(contactView.Render(w, nil))
+// }
+
+// func NotFound(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html")
+// 	w.WriteHeader(http.StatusNotFound)
+// 	fmt.Fprint(w, "<h1>no found bro</h1>")
+// }
+
+func main() {
+
+	// 	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	// 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	usersC := controllers.NewUsers()
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/movie", controllers.CreateMovie).Methods("POST")
+	r.HandleFunc("/api/movies", controllers.GetMyAllMovies).Methods("GET")
+	r.HandleFunc("/api/movie/{id}", controllers.MarkAsWatched).Methods("PUT")
+
+	r.HandleFunc("/api/movie/{id}", controllers.DeleteAMovie).Methods("DELETE")
+	r.HandleFunc("/api/deleteallmovie", controllers.DeleteAllMovies).Methods("DELETE")
+
+	staticC := controllers.NewStatic()
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
+
+	// r.HandleFunc("/", home).Methods("GET")
+	// r.HandleFunc("/contact", contact).Methods("GET")
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
+	r.HandleFunc("/signup", usersC.Create).Methods("POST")
+	// r.HandleFunc("/faq", faq)
+	// r.NotFoundHandler = http.HandlerFunc(NotFound)
+	r.Handle("/login", usersC.LoginView).Methods("GET")
+	r.HandleFunc("/login", usersC.Login).Methods("POST")
+	var u models.Users
+	u.Password = "hello there"
+	lx := models.UserService{}
+	controllers.CreateOrder(u, 1001, "Fake description #1")
+	y := models.Order{
+		UserID:      "abcd",
+		Amount:      60,
+		Description: "straing from main",
+	}
+	u.Orders = []models.Order{y}
+	inserted, err := lx.Create(&u)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted from outside", inserted.Email)
+	r.HandleFunc("/cookietest", usersC.CookieTest).Methods("GET")
+	http.ListenAndServe(":3000", r)
+
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 
 }
